538acb3c8832a8f504b8eac3a5806a2865e8fa41
blablamedia <pierre@gambaud.com>
2019-12-16 17:01:18 +0100

    iteration 3: done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,10 +1,10 @@
 const coindeskApi = axios.create({
-    baseURL: `https://api.coindesk.com/v1/bpi/historical/close.json`
+    baseURL: `https://api.coindesk.com/v1/bpi/historical/`
 });
 
-function getCoinValue() {
+function getCoinValue(startDate, endDate) {
     coindeskApi
-        .get()
+        .get(`close.json?start=${startDate}&end=${endDate}`)
         .then(coinValue => {
             const dailyData = coinValue.data.bpi;
             const coinDates = Object.keys(dailyData);
@@ -28,4 +28,34 @@ function getCoinValue() {
         .catch(err => console.error(err))
 }
 
-getCoinValue();
+
+// set initial values to date inputs
+const todayObj = new Date();
+const oneWeekAgoObj = new Date(new Date().setDate(new Date().getDate()-7));
+
+const todayYear = todayObj.getFullYear();
+const todayMonth = ('0' + (todayObj.getMonth() + 1)).slice(-2);
+const todayDay = ('0' + todayObj.getDate()).slice(-2);
+
+const oneWeekAgoYear = oneWeekAgoObj.getFullYear();
+const oneWeekAgoMonth = ('0' + (oneWeekAgoObj.getMonth() + 1)).slice(-2);
+const oneWeekAgoDay = ('0' + oneWeekAgoObj.getDate()).slice(-2);
+
+const today = `${todayYear}-${todayMonth}-${todayDay}`;
+const oneWeekAgo = `${oneWeekAgoYear}-${oneWeekAgoMonth}-${oneWeekAgoDay}`;
+
+document.getElementById(`startDate`).value = oneWeekAgo;
+document.getElementById(`endDate`).value = today;
+getCoinValue(oneWeekAgo, today);
+
+
+// add a listener on each input with .eventListener and update the graph in case of an event
+[...document.getElementsByClassName(`eventListener`)].forEach(input => 
+    {
+        input.addEventListener(`change`, () => {
+            const $startDate = document.getElementById(`startDate`).value;
+            const $endDate = document.getElementById(`endDate`).value;
+            getCoinValue($startDate, $endDate)
+        });
+    }
+)
