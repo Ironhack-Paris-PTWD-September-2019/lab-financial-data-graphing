77de7d33e75a9aba588b6a9bb0349a240a411767
blablamedia <pierre@gambaud.com>
2019-12-16 14:34:02 +0100

    iteration 2: done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -5,7 +5,26 @@ const coindeskApi = axios.create({
 function getCoinValue() {
     coindeskApi
         .get()
-        .then(coinData => console.log(coinData.data.bpi))
+        .then(coinValue => {
+            const dailyData = coinValue.data.bpi;
+            const coinDates = Object.keys(dailyData);
+            const coinValues = Object.values(dailyData);
+
+            console.log(coinDates, coinValues)
+
+            const $ctx = document.getElementById(`myChart`).getContext(`2d`);
+            const chart = new Chart($ctx, {
+                type: `line`,
+                data: {
+                    labels: coinDates,
+                    datasets: [{
+                        label: `Bitcoin Price Index`,
+                        data: coinValues
+                    }]   
+                }
+            })
+            console.log(dailyData)
+        })
         .catch(err => console.error(err))
 }
 
